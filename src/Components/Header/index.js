dfed879a3010c0c35b60b64f78ceec0ad5995817
Chuhj <chj9574@naver.com>
2021-12-10 17:55:38 +0900

    eslint 설정 적용

---
+++ src/Components/Header/index.js
@@ -0,0 +1,37 @@
+import React, { useCallback } from 'react';
+import styled from 'styled-components';
+import { useDispatch, useSelector } from 'react-redux';
+import { USER_TEST_REDUCER } from '../../Settings/Redux/Action/user';
+import { Ico_Logo, Ico_Search } from '../Assets';
+import {
+  HeaderSection,
+  IconBox,
+  LoginButton,
+  SearchIconWrapper,
+} from './style';
+
+// TODO
+// HEADERTEXT -> 이미지로 변경해야됨 로고가 글자인줄알았는데 svg파일이였음.
+const Header = () => {
+  const dispatch = useDispatch();
+
+  const dispatchEvenet = useCallback(() => {
+    dispatch({
+      type: USER_TEST_REDUCER,
+    });
+  }, []);
+
+  return (
+    <HeaderSection>
+      <img src={Ico_Logo} width={71} height={24} alt="" />
+      <IconBox>
+        <SearchIconWrapper>
+          <img width={18} height={18} src={Ico_Search} alt="" />
+        </SearchIconWrapper>
+        <LoginButton onClick={dispatchEvenet}>로그인</LoginButton>
+      </IconBox>
+    </HeaderSection>
+  );
+};
+
+export default Header;
