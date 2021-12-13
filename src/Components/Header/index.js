1d78548d0b4328b27ef6d49dd4168d84f5887a75
Chuhj <chj9574@naver.com>
2021-12-13 23:52:46 +0900

    feat: 글 작성, 유저 메뉴 컴포넌트 추가

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,19 +1,29 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useState, useEffect } from 'react';
 import styled from 'styled-components';
 import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 import { USER_TEST_REDUCER } from '../../Settings/Redux/Action/user';
-import { Ico_Logo, Ico_Search } from '../Assets';
+import {
+  Ico_Logo,
+  Ico_Search,
+  Ico_User_Default,
+  Ico_Dropdown,
+} from '../Assets';
 import {
   HeaderSection,
   IconBox,
   LoginButton,
   SearchIconWrapper,
+  UserWrapper,
+  UserMenuList,
+  WriteButton,
 } from './style';
 
 // TODO
 // HEADERTEXT -> 이미지로 변경해야됨 로고가 글자인줄알았는데 svg파일이였음.
 const Header = () => {
   const dispatch = useDispatch();
+  const [isOpen, setIsOpen] = useState(false);
 
   const dispatchEvenet = useCallback(() => {
     dispatch({
@@ -21,14 +31,46 @@ const Header = () => {
     });
   }, []);
 
+  const toggleIsOpen = useCallback(() => {
+    setIsOpen(!isOpen);
+    console.log(isOpen);
+  }, [isOpen]);
+
+  const handleCloseModal = useCallback(() => {
+    if (isOpen) {
+      setIsOpen(false);
+    }
+  }, [isOpen]);
+
+  useEffect(() => {
+    window.addEventListener('click', handleCloseModal);
+    return () => {
+      window.removeEventListener('click', handleCloseModal);
+    };
+  }, [isOpen]);
+
   return (
     <HeaderSection>
-      <img src={Ico_Logo} width={71} height={24} alt="" />
+      <Link to="/">
+        <img src={Ico_Logo} height={24} alt="" className="Logo" />
+      </Link>
       <IconBox>
-        <SearchIconWrapper>
+        <SearchIconWrapper to="/search">
           <img width={18} height={18} src={Ico_Search} alt="" />
         </SearchIconWrapper>
-        <LoginButton onClick={dispatchEvenet}>로그인</LoginButton>
+        <WriteButton>새 글 작성</WriteButton>
+        <UserWrapper onClick={toggleIsOpen}>
+          <img src={Ico_User_Default} className="user" alt="" />
+          <img src={Ico_Dropdown} className="down" alt="" />
+          <UserMenuList isOpen={isOpen}>
+            <Link to="/">내 벨로그</Link>
+            <Link to="/">임시 글</Link>
+            <Link to="/">읽기 목록</Link>
+            <Link to="/">설정</Link>
+            <Link to="/">로그아웃</Link>
+          </UserMenuList>
+        </UserWrapper>
+        {/* <LoginButton onClick={dispatchEvenet}>로그인</LoginButton> */}
       </IconBox>
     </HeaderSection>
   );
--- src/Components/Header/style.js
+++ src/Components/Header/style.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
 const HeaderSection = styled.div`
-  background-color: beige;
   width: 1029px;
   margin: 0 auto 0 auto;
   height: 64px;
@@ -9,8 +9,26 @@ const HeaderSection = styled.div`
   justify-content: space-between;
   align-items: center;
 
+  .Logo {
+    @media (min-width: 1025px) {
+      width: 28px;
+      height: 28px;
+    }
+  }
+
+  @media (min-width: 1920px) {
+    width: 1728px;
+  }
+  @media (max-width: 1919px) {
+    width: 1376px;
+  }
+
   @media (max-width: 1440px) {
-    width: 1029px;
+    width: 1024px;
+  }
+
+  @media (max-width: 1057px) {
+    width: 97%;
   }
 `;
 
@@ -25,18 +43,19 @@ const LoginButton = styled.button`
   height: 32px;
   border-radius: 1rem;
   border: none;
-  background-color: rgb(52, 58, 64);
+  background-color: ${({ theme }) => theme.black};
   transition: all 0.125s ease-in 0s;
   color: white;
   font-size: 16px;
   font-weight: bold;
+
   &:hover {
     cursor: pointer;
     opacity: 0.6;
   }
 `;
 
-const SearchIconWrapper = styled.div`
+const SearchIconWrapper = styled(Link)`
   display: flex;
   width: 2.5rem;
   height: 2.5rem;
@@ -44,10 +63,100 @@ const SearchIconWrapper = styled.div`
   margin-right: 0.75rem;
   align-items: center;
   justify-content: center;
+
   &:hover {
     cursor: pointer;
     background-color: rgba(0, 0, 0, 0.043);
   }
 `;
 
-export { HeaderSection, IconBox, LoginButton, SearchIconWrapper };
+const UserWrapper = styled.div`
+  width: 4rem;
+  height: 2rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  position: relative;
+  cursor: pointer;
+
+  &:hover {
+    img {
+      filter: none;
+    }
+  }
+
+  .user {
+    width: 40px;
+    height: 40px;
+    border-radius: 50%;
+  }
+
+  .down {
+    width: 22px;
+    height: 22px;
+    filter: invert(61%) sepia(10%) saturate(280%) hue-rotate(169deg)
+      brightness(89%) contrast(93%);
+  }
+`;
+
+const UserMenuList = styled.div`
+  z-index: 1;
+  display: none;
+  width: 12rem;
+  list-style: none;
+  position: absolute;
+  top: 3.2rem;
+  right: 0;
+  margin: 0;
+  padding-left: 0;
+  background-color: white;
+  box-shadow: 0px 0px 5px 2px rgba(0, 0, 0, 0.08);
+
+  ${({ isOpen }) =>
+    isOpen &&
+    `
+      display: block;
+    `}
+
+  a {
+    display: block;
+    text-decoration: none;
+    color: black;
+    height: 3rem;
+    line-height: 3rem;
+    padding-left: 1rem;
+
+    &:hover {
+      background-color: ${({ theme }) => theme.hover};
+    }
+  }
+`;
+
+const WriteButton = styled(LoginButton)`
+  width: 6.5rem;
+  border: 1px solid ${({ theme }) => theme.black};
+  margin-right: 1rem;
+  text-align: center;
+  color: ${({ theme }) => theme.black};
+  background-color: white;
+
+  &:hover {
+    opacity: 1;
+    color: white;
+    background-color: ${({ theme }) => theme.black};
+  }
+
+  @media (max-width: 1024px) {
+    display: none;
+  }
+`;
+
+export {
+  HeaderSection,
+  IconBox,
+  LoginButton,
+  SearchIconWrapper,
+  UserWrapper,
+  UserMenuList,
+  WriteButton,
+};
