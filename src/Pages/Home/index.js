280c594109735fcbc205ae2d390f946beae8ca55
Chuhj <chj9574@naver.com>
2021-12-14 00:24:18 +0900

    Home backgound-color 변경

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import styled from 'styled-components';
 import Header from '../../Components/Header';
+import MainBody from '../../Components/MainBody';
+import ScrollHeader from '../../Components/ScrollHeader';
 
 const HomeContainer = styled.div`
   display: flex;
@@ -10,12 +12,20 @@ const HomeContainer = styled.div`
   justify-content: center;
   align-items: center;
   flex-direction: column;
+  background-color: #f8f9fa;
+
+  a {
+    color: black;
+    text-decoration: none;
+  }
 `;
 
 const Home = () => {
   return (
     <HomeContainer>
+      <ScrollHeader />
       <Header />
+      <MainBody />
     </HomeContainer>
   );
 };
