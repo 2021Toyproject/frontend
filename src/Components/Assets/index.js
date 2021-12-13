8115a0bca00208ceb3457ef0b66173fde632a19d
Chuhj <chj9574@naver.com>
2021-12-13 23:44:02 +0900

    이미지 추가

--- src/Components/Assets/index.js
+++ src/Components/Assets/index.js
@@ -6,5 +6,19 @@ import Ico_Share from './Img/share.svg';
 import Ico_Time from './Img/Time.svg';
 import Ico_Trend from './Img/Trend.svg';
 import Ico_Logo from './Img/v_logo.svg';
+import Ico_More from './Img/more.svg';
+import Ico_Dropdown from './Img/dropdown.svg';
+import Ico_User_Default from './Img/user_default.png';
 
-export { Ico_Ribbon, Ico_Search, Ico_Share, Ico_Time, Ico_Trend, Ico_Logo };
+export {
+  Ico_Ribbon,
+  Ico_Search,
+  Ico_Share,
+  Ico_Time,
+  Ico_Trend,
+  Ico_Logo,
+  Ico_HeartBlack,
+  Ico_More,
+  Ico_Dropdown,
+  Ico_User_Default,
+};
