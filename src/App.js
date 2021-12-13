b793841af28481763e64718faacc640ac559505f
Chuhj <chj9574@naver.com>
2021-12-14 00:03:52 +0900

    styled-components Theme 추가

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import React from 'react';
+import { ThemeProvider } from 'styled-components';
 import { Route, Routes } from 'react-router-dom';
 import Home from './Pages/Home';
+import Search from './Pages/Search';
+
+const theme = {
+  black: '#343a40',
+  grey: '#868E96',
+  green: '#12B886',
+  hover: '#f7f7f8',
+};
 
 function App() {
   return (
     <div className="App">
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
+      <ThemeProvider theme={theme}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/search" element={<Search />} />
+        </Routes>
+      </ThemeProvider>
     </div>
   );
 }
